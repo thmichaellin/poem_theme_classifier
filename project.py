22cac83675af85767be03b89f53c8dc354a2633f
skratzke <129890737+skratzke@users.noreply.github.com>
2024-11-26 17:34:28 +0100

    Update project.py

--- project.py
+++ project.py
@@ -3,10 +3,4 @@ import pandas as pd
 data = pd.read_csv("./data/PoetryFoundationData.csv")
 
 print(data.head())
-<<<<<<< Updated upstream
-=======
 
-# push it ah 
-
-# second change
->>>>>>> Stashed changes
