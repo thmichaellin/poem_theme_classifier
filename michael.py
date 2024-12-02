38bb24e015815c13a0aa3d88b52acabae756517c
Michael Lin <thmichaellin@gmail.com>
2024-12-02 23:40:17 +0100

    Labels Distribution
    Dict and Plot

--- michael.py
+++ michael.py
@@ -1,19 +1,26 @@
 from project import *
 import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = pd.read_csv("./data/PoetryFoundationData.csv")
+data = pd.read_csv('./data/PoetryFoundationData.csv', usecols=['Poem', 'Tags'])
+data = data.dropna()
 
-print(data.head())
+tag_dict = Counter()
 
-tokenizer = DistilBertTokenizer.from_pretrained(
-    "distilbert-base-uncased-finetuned-sst-2-english")
-model = DistilBertForSequenceClassification.from_pretrained(
-    "distilbert-base-uncased-finetuned-sst-2-english")
+for tags in data['Tags']:
+    tag_list = tags.replace(" & ", ",").split(',')
+    tag_dict.update(tag_list)
+    
+#tags_sorted = dict(sorted(tag_dict.items(), key=lambda item: item[1], reverse=True))
+#print(tags_sorted)
 
-inputs = tokenizer(data["Poem"][0], return_tensors="pt")
-with torch.no_grad():
-    logits = model(**inputs).logits
-
-predicted_class_id = logits.argmax().item()
-model.config.id2label[predicted_class_id]
+tag_df = pd.DataFrame(tag_dict.items(), columns=['Tag', 'Count'])
+tag_df = tag_df.sort_values(by='Count', ascending=False)
+sns.barplot(x='Tag', y='Count', data=tag_df.head(30))
+plt.xticks(rotation=45, ha='right')
+plt.xlabel('Tags')
+plt.ylabel('Frequency')
+plt.show()
