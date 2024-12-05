7f28af36aadd77353d192d88ff1a2af84933f496
thmichaellin <thmichaellin@gmail.com>
2024-12-05 14:56:43 +0100

    Tag Dictionary Pipeline

--- classes.py
+++ classes.py
@@ -7,15 +7,19 @@ class PoetryData:
     def __init__(self, path: str = './data/PoetryFoundationData.csv',
                  cols: list = ['Poem', 'Tags']):
         self.data = self.load_poems(path, cols)
-
+        self.translate_dict = {}
         self.parse_text()
         self.parse_tags()
+        self.exploded_tags = self.data['Tags'].explode()
         self.count_tags()
 
     def load_poems(self, path: str, cols: list) -> pd.DataFrame:
         data = pd.read_csv(path, usecols=cols)
         data = data.dropna()
         return data
+
+    def load_translate_dict(self, translate_dict: dict) -> None:
+        self.translate_dict = translate_dict
 
     def parse_text(self) -> None:
         self.data['Poem'] = (self.data['Poem']
@@ -29,9 +33,15 @@ class PoetryData:
                              .str.split(','))
 
     def count_tags(self) -> None:
-        self.tags = self.data['Tags'].explode().value_counts()
+        self.tags = self.exploded_tags.value_counts()
         self.tags = self.tags.reset_index()
         self.tags.columns = ['Tag', 'Count']
+
+    def translate_tags(self) -> None:
+        self.exploded_tags = self.exploded_tags.replace(self.translate_dict)
+        self.data['Tags'] = self.exploded_tags.groupby(
+            self.exploded_tags.index).apply(
+                list)
 
     def plot_tags(self, num_tags: int = 30) -> None:
         sns.barplot(x='Tag', y='Count', data=self.tags.head(num_tags))
--- michael.py
+++ michael.py
@@ -5,7 +5,6 @@ from classes import *
 if __name__ == "__main__":
     poetry = PoetryData('./data/PoetryFoundationData.csv', ['Poem', 'Tags'])
     # poetry.plot_tags(30)
+
     print(poetry.tags)
     # print(poetry.data)
-
-# Explore colinearity between labels.
