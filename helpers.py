215ccb78025021a6ff21743a7db3aa56960739c6
thmichaellin <thmichaellin@gmail.com>
2024-12-11 16:26:20 +0100

    Helper Functions
    Moved helpers from notebook to script

---
+++ helpers.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+
+
+def hamming_score(y_true, y_pred, normalize=True, sample_weight=None):
+    acc_list = []
+    for i in range(y_true.shape[0]):
+        set_true = set(np.where(y_true[i])[0])
+        set_pred = set(np.where(y_pred[i])[0])
+        tmp_a = None
+        if len(set_true) == 0 and len(set_pred) == 0:
+            tmp_a = 1
+        else:
+            tmp_a = len(set_true.intersection(set_pred)) /\
+                float(len(set_true.union(set_pred)))
+        acc_list.append(tmp_a)
+    return np.mean(acc_list)
+
+
+def loss_fn(outputs, targets):
+    return torch.nn.BCEWithLogitsLoss()(outputs, targets)
+
+
+def train(epoch, device, model, optimizer, training_loader, scaler):
+    model.train()
+    for _, data in tqdm(enumerate(training_loader, 0)):
+        ids = data['ids'].to(device, dtype=torch.long)
+        mask = data['mask'].to(device, dtype=torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+        targets = data['targets'].to(device, dtype=torch.float)
+
+        optimizer.zero_grad()
+        with torch.cuda.amp.autocast():
+            outputs = model(ids, mask, token_type_ids)
+            loss = loss_fn(outputs, targets)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+
+def validation(testing_loader, model):
+    model.eval()
+    fin_targets = []
+    fin_outputs = []
+    with torch.no_grad():
+        for _, data in tqdm(enumerate(testing_loader, 0)):
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
+            token_type_ids = data['token_type_ids'].to(
+                device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype=torch.float)
+            outputs = model(ids, mask, token_type_ids)
+            fin_targets.extend(targets.cpu().detach().numpy().tolist())
+            fin_outputs.extend(torch.sigmoid(
+                outputs).cpu().detach().numpy().tolist())
+    return fin_outputs, fin_targets
