6d6b616c6a759d4b74f3c5fc2363cc1022e687b4
thmichaellin <thmichaellin@gmail.com>
2024-12-12 15:40:16 +0100

    Refactoring Classes

--- classes.py
+++ classes.py
@@ -95,13 +95,18 @@ class PoetryData:
 
 class PoetrySubset(PoetryData):
     def __init__(self, parent: PoetryData,
-                 test_size: float = 0.2,
+                 test_size: float = 0.3,
                  random_state: int = 10):
         self.data = parent.data
         self.process_labels()
-        self.train_data, self.val_data = train_test_split(
+        self.train_data, self.val_test_data = train_test_split(
             self.data,
             test_size=test_size,
+            random_state=random_state
+        )
+        self.val_data, self.test_data = train_test_split(
+            self.data,
+            test_size=test_size / 2,
             random_state=random_state
         )
 
